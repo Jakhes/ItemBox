2289a9846b05a69d3d941edabbc6835cfbea3032
Braumble <dean.schmitz@schmitzbauer.de>
2024-01-02 14:31:12 +0100

    Adding Connector Interfaces for Saving Models

---
+++ ItemLibraray/GlobalConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemLibraray
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+
+        public static void Init_Connections(bool database, bool textFile)
+        {
+            if (database)
+            {
+                // TODO - Create Database Connection
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+
+            if (textFile)
+            {
+                // TODO - Create Textfile Connection
+                TextFileConnector text = new TextFileConnector();
+                Connections.Add(text);
+            }
+        }
+    }
+}
---
+++ ItemLibraray/IDataConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemLibraray
+{
+    public interface IDataConnection
+    {
+        Item Create_Item(Item model);
+    }
+}
