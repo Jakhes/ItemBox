d1212029e3cc72ed82cf00705c426baa78ebee17
deans <deans@GREATKRAKEN>
2024-01-02 01:21:41 +0100

    Projektdateien hinzufügen.

---
+++ ItemLibraray/Item.cs
@@ -0,0 +1,26 @@
+﻿namespace ItemLibraray
+{
+    public class Item
+    {
+        /// <summary>
+        /// The Items Name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A short description of the Item.
+        /// </summary>
+        public string Description { get; set; }
+
+        // public int Image { get; set; } need to find out how to add Images
+        /// <summary>
+        /// Represents how much the Item is worth in a numeric currency.
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Represents the Lore that might be hidden around the Item.
+        /// </summary>
+        public string Lore { get; set; }
+    }
+}
---
+++ ItemLibraray/System.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemLibraray
+{
+    internal class System
+    {
+        /// <summary>
+        /// The System's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Represents a List of all Items in a cohesive System.
+        /// </summary>
+        public List<Item> ItemCodex { get; set; } = new List<Item>();
+
+    }
+}
